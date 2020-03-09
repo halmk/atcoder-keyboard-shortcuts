bd32e1bd4953ff83d6b09f5fe8a52d0a88a5c0d4
halmk <elle2206@i.softbank.jp>
2020-03-09 10:44:43 +0900

    Add copy sample shortcuts

---
+++ script.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         atcoder-keyboard-shortcuts
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  atcoder keyboard shortcuts
+// @author       springroll
+// @match        https://atcoder.jp/contests/*/tasks/*
+// @require       https://unpkg.com/hotkeys-js/dist/hotkeys.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var copySample = function(index) {
+        window.getSelection().removeAllRanges();
+        try {
+            var range = document.createRange();
+            range.selectNode($('#pre-sample'+(2*(index-1))).get(0));
+            window.getSelection().addRange(range);
+            document.execCommand('copy');
+        } catch (err) {
+            console.log(err);
+        }
+        alert("Copied Sample" + index + ".");
+        window.getSelection().removeAllRanges();
+    }
+    hotkeys('shift+1,shift+2,shift+3,shift+4,shift+5', function (event, handler){
+        switch (handler.key) {
+            case 'shift+1':
+                copySample(1);
+                break;
+            case 'shift+2':
+                copySample(2);
+                break;
+            case 'shift+3':
+                copySample(3);
+                break;
+            case 'shift+4':
+                copySample(4);
+                break;
+            case 'shift+5':
+                copySample(5);
+                break;
+            default: alert(event);
+        }
+    });
+
+})();
